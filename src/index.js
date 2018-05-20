9c6563b7af7b967bb20b5e9ddc73e642aeac22de
killofki <killofki@yahoo.co.kr>
2018-05-20 22:54:36 +0900

    dot tabs..

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ var { getOptions } = require('loader-utils')
 
 module .exports = function ( source ) { 
 	var options = getOptions( this ) 
-	var name = options.name || '[name]-[index]' 
-	var illustratorClass = options.illustratorClass || 'cls' 
-	var remove = options.remove || {} 
-	var exportStyle = options.exportStyle || 'default' 
-	var defaultName = options.defaultName 
+	var name = options .name || '[name]-[index]' 
+	var illustratorClass = options .illustratorClass || 'cls' 
+	var remove = options .remove || {} 
+	var exportStyle = options .exportStyle || 'default' 
+	var defaultName = options .defaultName 
 	
 	if (exportStyle === 'name' && !defaultName) { 
 		throw new Error('if exportStyle is a name, defaultName is required') 
@@ -16,10 +16,10 @@ module .exports = function ( source ) {
 		throw new Error('if exportStyle is not a name, defaultName is not needed') 
 		} 
 	
-	source = JSON.stringify(source) 
+	source = JSON .stringify(source) 
 	
 	var titlePattern = /<title>(.+)<\/title>/ 
-	var match = source.match(titlePattern) 
+	var match = source .match(titlePattern) 
 	var iconName = (match) ? match[1] : defaultName 
 	
 	source = 
@@ -33,15 +33,15 @@ module .exports = function ( source ) {
 			} 
 			) 
 	
-	if (remove.title) { 
-		source = source.replace(titlePattern, '') 
+	if (remove .title) { 
+		source = source .replace(titlePattern, '') 
 		} 
 	
-	if (remove.xmlns) { 
-		source = source.replace(' xmlns="http://www.w3.org/2000/svg"', '') 
+	if (remove .xmlns) { 
+		source = source .replace(' xmlns="http://www.w3.org/2000/svg"', '') 
 		} 
 	
-	if (remove.space) { 
+	if (remove .space) { 
 		source = 
 			source 
 			.replace(/[\r\n]+/g, '') 
@@ -53,5 +53,5 @@ module .exports = function ( source ) {
 		  (exportStyle === 'none') ? '' 
 		: (exportStyle === 'name') ? '.' + iconName 
 		: '.default' 
-	return 'module.exports' + exportStyle + ' = ' + source 
+	return 'module .exports' + exportStyle + ' = ' + source 
 	} 
